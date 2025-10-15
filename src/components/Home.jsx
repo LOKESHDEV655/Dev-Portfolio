ce1e70b3daa20f2b75cdcd0994488f8adad64bef
LOKESHDEV655 <lokeshdev655@gmail.com>
2025-10-16 00:34:25 +0530

    pdf added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
           <div className="home__buttons">
             <a
               download
-              href="assets/pdf/Gautam_Final_Resume.pdf"
+              href="assets/pdf/Lokesh_dev_resume.pdf"
               className="button button--ghost"
             >
               Download Resume
